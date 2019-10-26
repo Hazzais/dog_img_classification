97f6fb7f061ad55fb7f67dc011e1373bbee0512a
Harry <harryafirth@gmail.com>
2019-10-26 14:07:30 +0100

    Cleaned up batch image download from ImageNet

--- imagenet.py
+++ imagenet.py
@@ -4,21 +4,16 @@ Created on Thu Jan  3 19:16:35 2019
 
 @author: harry
 """
+import os
+import argparse
+import warnings
 
 from bs4 import BeautifulSoup
 import numpy as np
 import requests
 import cv2
 import urllib
-import os
 
-page = requests.get("http://www.image-net.org/api/text/imagenet.synset.geturls?wnid=n02099712")
-
-soup = BeautifulSoup(page.content, 'html.parser')
-
-str_soup=str(soup)#convert soup to string so it can be split
-
-split_urls=str_soup.split('\r\n')#split so each url is a different possition on a list
 
 def url_to_image(url):
     # download the image, convert it to a NumPy array, and then read
@@ -31,38 +26,72 @@ def url_to_image(url):
     return image
 
 
-image_base_folder = 'D:\Documents\PythonDoc\Photo_classification\Images\Other_sources\ImageNet\Dogs'
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Download a number of images "
+                                                 "for a WordNet Id into a "
+                                                 "folder")
+    parser.add_argument('wordnet_id', type=str,
+                        help='The WordNet Id to download images for')
+    parser.add_argument('out_path', type=str,
+                        help='The location to save images')
+    parser.add_argument('-n', '--n-images', default=50, type=int,
+                        help='The number of images to attempt to download')
+    parser.add_argument('-x', '--x-pixels', default=64, type=int,
+                        help="Number of x-pixels to convert each image to")
+    parser.add_argument('-y', '--y-pixels', default=64, type=int,
+                        help="Number of y-pixels to convert each image to")
+    parser.add_argument('-f', '--file-extension', default='jpg',
+                        choices=['jpg', 'png'],
+                        help="Extension to save images with")
+    parser.add_argument('-b', '--base_url', type=str,
+                        default='http://www.image-net.org/api/text/'
+                                'imagenet.synset.geturls?wnid=',
+                        help='The number of x pixels to convert the image to')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help="Print progress every 20 images")
+    parser.set_defaults(verbose=False)
+    args = parser.parse_args()
+    img_url = "{}{}".format(args.base_url, args.wordnet_id)
 
-img_rows, img_cols = 64, 64 #number of rows and columns to convert the images to
-input_shape = (img_rows, img_cols, 3)#format to store the images (rows, columns,channels) called channels last
+    page = requests.get(img_url)
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-n_of_training_images=50
-for progress in range(n_of_training_images):
-    # Print out progress whenever progress is a multiple of 20 so we can follow the
-    # (relatively slow) progress
-    if(progress%20==0):
-        print(progress)
-    if not split_urls[progress] == None:
-        try:
-            I = url_to_image(split_urls[progress])
-            #check if the image has width, length and channels
-            if (len(I.shape))==3:
-                #create a name of each image
-                save_path = os.path.join(image_base_folder,str(progress)+'.jpg')
-                cv2.imwrite(save_path,I)
-        except:
-            None
+    # Need to be able to strip the soup so individual URLs can be different
+    # elements of a list
+    str_soup = str(soup)
+    split_urls = str_soup.split('\r\n')
 
+    image_base_folder = args.out_path
+    img_extension = args.file_extension
 
+    img_rows, img_cols = args.x_pixels, args.y_pixels
 
-urls_map = os.path.join(image_base_folder,'imagenet_fall11_urls','fall11_urls.txt')
+    # Third argument is number of channels - keep as 3 to keep colour
+    input_shape = (img_rows, img_cols, 3)
 
-txt=[]
-with open(urls_map,'rb') as f:
-    for line in f:
-        txt.append(line)
+    n_of_training_images = args.n_images
 
+    imgs_no_load = []
+    for i_img in range(n_of_training_images):
 
-small = txt[:500]
+        # Optionally print out i_img whenever i_img is a multiple of 20
+        # so we can follow the (relatively slow) process
+        if i_img % 20 == 0:
+            print(i_img)
+        if split_urls[i_img]:
+            try:
+                img_contents = url_to_image(split_urls[i_img])
+                # check if the image has width, length and channels
+                if len(img_contents.shape) == 3:
+                    # create a name of each image
+                    save_path = os.path.join(image_base_folder,
+                                             '{}.{}'.format(i_img,
+                                                            img_extension))
+                    cv2.imwrite(save_path, img_contents)
+            except:
+                imgs_no_load.append(split_urls[i_img])
 
-
+    no_load = len(imgs_no_load)
+    if no_load > 0:
+        warnings.warn("There were {} images which could not be downloaded"
+                      .format(no_load))
